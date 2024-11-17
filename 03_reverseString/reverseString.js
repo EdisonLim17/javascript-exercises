a301e2cb1e724220df2e3d1731c52affe5cc6d03
Edison Lim <117683125+EdisonLim17@users.noreply.github.com>
2024-11-17 18:19:59 -0500

    Completed 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(s) {
+    let r = "";
+    for(let i = s.length - 1; i >= 0; i--){
+        r += s[i];
+    }
+    return r;
 };
 
 // Do not edit below this line
