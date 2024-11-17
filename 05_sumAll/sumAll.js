86b37871c5212036b715d0a4460df4fd8d16482f
Edison Lim <117683125+EdisonLim17@users.noreply.github.com>
2024-11-17 18:34:49 -0500

    Completed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)){
+        return 'ERROR';
+    }
+    sum = 0;
+    if (a < b){
+        for (let i = a; i <= b; i++){
+            sum += i;
+        }
+        return sum;
+    }
+    for (let i = b; i <= a; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
