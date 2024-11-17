b6953bc48622c938e9058f90168c1be490fc6909
Edison Lim <117683125+EdisonLim17@users.noreply.github.com>
2024-11-17 18:29:02 -0500

    Completed 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(n, ...args) {
+    a = [];
+    for (const element of n){
+        if (!args.includes(element)) a.push(element);
+    }
+    return a;
 };
 
 // Do not edit below this line
