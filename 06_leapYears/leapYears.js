df132678ba9c5ea1023be19996180a340f9cce1f
Edison Lim <117683125+EdisonLim17@users.noreply.github.com>
2024-11-17 18:39:36 -0500

    Completed 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(n) {
+    if (n % 400 === 0) return true;
+    if (n % 100 === 0) return false;
+    if (n % 4 === 0) return true;
+    return false;
 };
 
 // Do not edit below this line
