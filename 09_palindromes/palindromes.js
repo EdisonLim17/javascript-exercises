0712f2a982e9bb827b45f79d125ddf1d0a0ced68
Edison Lim <117683125+EdisonLim17@users.noreply.github.com>
2024-12-15 01:55:49 -0500

    Completed 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const arr = s.toLowerCase().split("");
+    const filtered = arr.filter(c => alphanumerical.includes(c)).join("");
+    const reversed = filtered.split("").reverse().join("");
+    return filtered === reversed;
 };
 
 // Do not edit below this line
