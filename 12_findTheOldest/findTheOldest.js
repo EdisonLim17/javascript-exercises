e1dbc8a07e8a04f7ece871232b73cf692368dba3
Edison Lim <117683125+EdisonLim17@users.noreply.github.com>
2024-12-15 02:30:46 -0500

    Completed 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+return arr.reduce((oldest, curr) => {
+        if(!oldest.yearOfDeath){
+            oldest.yearOfDeath = (new Date()).getFullYear();
+        }
+        if(!curr.yearOfDeath){
+            curr.yearOfDeath =  (new Date()).getFullYear();
+        }
+        if(curr.yearOfDeath - curr.yearOfBirth > oldest.yearOfDeath - oldest.yearOfBirth){
+            oldest = curr;
+        }
+        return oldest;
+    })
 };
 
 // Do not edit below this line
