c759d76f94e88f6b6624544e509dd129d6d34e20
Edison Lim <117683125+EdisonLim17@users.noreply.github.com>
2024-12-15 01:37:13 -0500

    Completed 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a -b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, n) => total + n, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, n) => total * n, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let p = 1;
+  for(let i = n; i < 0; i--){
+    p *= n;
+  }
+  return p;
 };
 
 // Do not edit below this line
